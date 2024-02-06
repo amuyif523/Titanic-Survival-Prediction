2c178efd711dfce1b022cc67d6891b5d21e1a01a
Amanuel Fikremariam <amanfikre523@gmail.com>
2024-02-06 15:00:00 +0300

    refactor(preprocessing): Move Age and Embarked imputation to script

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,4 +2,5 @@ import pandas as pd
 # Initial structure for preprocessing function
 def preprocess_data(df, is_train=True):
     df_processed = df.copy()
+        # Age Imputation
     return df_processed
